028ca88dfc1bd1c8484edbfd08b679fc47185498
Pedro Cardona <pedropca@stud.ntnu.no>
2024-05-31 15:16:51 +0200

    Refactor TSPDataParser and TSMPModel classes

--- travelling_salesman_problem/ML_model.py
+++ travelling_salesman_problem/ML_model.py
@@ -6,21 +6,12 @@ import numpy as np
 from torch import nn
 from Path_generator import get_path
 
-
-
 class TSPDataParser(Dataset):
     def __init__(self, data_dir, split='train'):
-        """
-        Args:
-            data_dir (string): Directory with all the numpy files.
-            split (string): One of 'train', 'val', 'test' to specify the data split.
-        """
         self.data_dir = data_dir
         self.split = split
         self.X_dir = os.path.join(data_dir, 'X-data', split)
         self.Y_dir = os.path.join(data_dir, 'Y-data', split)
-        
-        # Get list of all file names (assuming both X and Y have same file names)
         self.file_names = [f for f in os.listdir(self.X_dir) if os.path.isfile(os.path.join(self.X_dir, f))]
     
     def __len__(self):
@@ -29,34 +20,16 @@ class TSPDataParser(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        
-        # Get file name
         file_name = self.file_names[idx]
-        
-        # Load X data
         X_path = os.path.join(self.X_dir, file_name)
         X = np.load(X_path)
         X = torch.tensor(X, dtype=torch.float32)
-        
-        # Load Y data
         Y_path = os.path.join(self.Y_dir, file_name)
         Y = np.load(Y_path)
         Y = torch.tensor(Y, dtype=torch.float32)
-        
         return X, Y
 
 def create_dataloader(data_dir, batch_size=64, split='train', shuffle=True, num_workers=0):
-    """
-    Args:
-        data_dir (string): Directory with all the numpy files.
-        batch_size (int): Batch size for the dataloader.
-        split (string): One of 'train', 'val', 'test' to specify the data split.
-        shuffle (bool): Whether to shuffle the data.
-        num_workers (int): Number of workers for data loading.
-        
-    Returns:
-        DataLoader: A PyTorch DataLoader for the specified data split.
-    """
     dataset = TSPDataParser(data_dir, split)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
     return dataloader
@@ -118,20 +91,9 @@ class ResidualBlock(nn.Module):
         out += self.shortcut(x)
         return nn.ReLU()(out)
 
-
 class TSMPModel(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim, num_blocks=4, num_heads=8, ff_dim=512):
-        """
-        Args:
-            input_dim (int): Dimension of the input feature vector.
-            hidden_dim (int): Dimension of the hidden layers.
-            output_dim (int): Dimension of the output feature vector.
-            num_blocks (int): Number of residual blocks to include.
-            num_heads (int): Number of attention heads in the transformer block.
-            ff_dim (int): Dimension of the feedforward network in the transformer block.
-        """
         super(TSMPModel, self).__init__()
-
         self.flatten = nn.Flatten()
         self.initial_block = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
@@ -139,13 +101,10 @@ class TSMPModel(nn.Module):
             nn.ReLU(),
             nn.Dropout(0.3)
         )
-
         self.blocks = nn.Sequential(
             *[ResidualBlock(hidden_dim, hidden_dim) for _ in range(num_blocks)]
         )
-
         self.transformer_block = TransformerBlock(hidden_dim, num_heads, ff_dim)
-        
         self.fc_final = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
@@ -164,11 +123,11 @@ def define_model():
     train_dataloader = create_dataloader(data_dir, split='train')
     one_batch = next(iter(train_dataloader))
     input_obj, output_obj = one_batch
-    input_dim = input_obj.view(input_obj.shape[0], -1).shape[1]  # Flatten the input to 1D
-    output_dim = output_obj.view(output_obj.shape[0], -1).shape[1]  # Flatten the output to 1D
-    hidden_dim = 128*4  # You can adjust this parameter based on your model complexity
+    input_dim = input_obj.view(input_obj.shape[0], -1).shape[1]
+    output_dim = output_obj.view(output_obj.shape[0], -1).shape[1]
+    hidden_dim = 128 * 4
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = TSMPModel(input_dim, hidden_dim, output_dim, num_blocks=16,num_heads=32).to(device)
+    model = TSMPModel(input_dim, hidden_dim, output_dim, num_blocks=16, num_heads=32, ff_dim=512).to(device)
     return model
 
 def main():
@@ -178,17 +137,11 @@ def main():
     val_dataloader = create_dataloader(data_dir, split='val')
     test_dataloader = create_dataloader(data_dir, split='test')
     model = define_model()
-
-    # Load the model
-    model.load_state_dict(torch.load(f'{path}/models/model.pth'))
-
     optimizer = torch.optim.Adam(model.parameters(), lr=0.00001, weight_decay=1e-5)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=True)
     loss_fn = nn.MSELoss()
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-    train(model, train_dataloader, val_dataloader, test_dataloader, optimizer, loss_fn, epochs=100, device=device)
-
-    #Save the model
+    train(model, train_dataloader, val_dataloader, test_dataloader, optimizer, loss_fn, epochs=100, device=device, scheduler=scheduler)
     torch.save(model.state_dict(), f'{path}/models/model.pth')
 
 if __name__ == '__main__':
